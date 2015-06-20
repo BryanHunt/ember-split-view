bc0d29ffd0b39bc3e7531b1bf5d53cf36a868924
Bryan Hunt <bhunt@mac.com>
2015-06-20 12:03:53 -0500

    Fix unit tests

--- tests/unit/components/split-child-test.js
+++ tests/unit/components/split-child-test.js
@@ -3,6 +3,8 @@ import {
   test
 } from 'ember-qunit';
 
+import Ember from 'ember';
+
 moduleForComponent('split-child', 'SplitChildComponent', {
   // specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar']
@@ -12,7 +14,12 @@ test('it renders', function(assert) {
   assert.expect(2);
 
   // creates the component instance
-  var component = this.subject();
+  var component;
+  
+  Ember.run(function(){
+    component = this.subject();
+  }.bind(this));
+
   assert.equal(component._state, 'preRender');
 
   // appends the component to the page
