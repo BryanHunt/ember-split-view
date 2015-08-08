96355c36690e4dd6682db111efb8281bc563dc03
Bryan Crotaz <bryan.crotaz@silvercurve.co.uk>
2015-08-08 09:00:08 +0100

    Fixed included styling by moving it to vendor/

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 module.exports = {
   name: 'ember-split-view',
   included: function(app) {
-   this._super.included(app);
- }
+    this._super.included(app);
+    app.import('vendor/ember-split-view/styles/split-view.css', { destDir: '/' });
+  }
 };
